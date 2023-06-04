1d2d1813aa36a34abcbaa66991b5b37ad2057c7f
Stepan <115095505+HappySaber@users.noreply.github.com>
2023-06-04 22:34:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,30 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <locale>
+
+#include "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  std::string words;
+  BST<std::string> peace;
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+  } else{
+    while (!file.eof()) {
+      char sym = tolower(file.get());
+      if (isalpha(sym)) {
+        words += sym;
+      } else {
+        if (!words.empty()) {
+          peace.add(words);
+        }
+        words.clear();
+      }
+    }
+  }
+  file.close();
+  return peace;
 }
